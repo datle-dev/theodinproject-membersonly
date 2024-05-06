ef8f4e4ee4deb01fa5553125ce47b303897765aa
datle-dev <dle.dev@outlook.com>
2024-05-06 11:55:02 -0400

    Fix error in render path for error view

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use((err, req, res, next) => {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('pages/error');
 });
 
 module.exports = app;
