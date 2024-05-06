aae997b1f4e2998d7d822543642302dd916d6ace
datle-dev <dle.dev@outlook.com>
2024-05-06 13:18:41 -0400

    Add luxon formatted date to Post schema

--- models/post.js
+++ models/post.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const { DateTime } = require('luxon');
 
 const PostSchema = new Schema({
   username: { type: String, required: true },
   text: { type: String, required: true },
   added: { type: Date, required: true },
+});
+
+PostSchema.virtual('dateFormatted').get(function () {
+  return DateTime.fromJSDate(this.added).toLocaleString(DateTime.DATE_MED);
 });
 
 module.exports = mongoose.model('Post', PostSchema);
