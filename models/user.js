58a4ce52717b0bab2e7be593ce9e6646cce114ca
datle-dev <dle.dev@outlook.com>
2024-05-06 08:37:11 -0400

    Add admin boolean to user schema

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const UserSchema = new Schema({
   username: { type: String, required: true },
   hash: { type: String, required: true },
   salt: { type: String, required: true },
+  admin: { type: Boolean, required: true },
 });
 
 module.exports = mongoose.model('User', UserSchema);
