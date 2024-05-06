2cf58aeff44622343b6ddb54dcb975a783cf5c84
datle-dev <dle.dev@outlook.com>
2024-05-06 07:26:23 -0400

    Initial commit of user schema file

---
+++ models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: { type: String, required: true },
+  hash: { type: String, required: true },
+  salt: { type: String, required: true },
+});
+
+module.exports = mongoose.model('User', UserSchema);
