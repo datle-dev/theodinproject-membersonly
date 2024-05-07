fb5d4c4a845e614aa05d28a1af5ad320942e6cb1
datle-dev <dle.dev@outlook.com>
2024-05-07 08:16:36 -0400

    Add more fields to user schema

--- models/user.js
+++ models/user.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
   username: { type: String, required: true },
   hash: { type: String, required: true },
   salt: { type: String, required: true },
